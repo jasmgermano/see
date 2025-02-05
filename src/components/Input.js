c68a4b9de737e34aaaab6b0caee01a0af0bb845b
jasmine <jasmigermano@gmail.com>
2025-02-05 17:41:57 -0300

    ✨ feat: allow passing ref prop

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function Input({ label, type, name, value, onChange, onBlur, error, placeholder }) {
+export default function Input({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  onBlur,
+  error,
+  placeholder,
+  ref
+}) {
   return (
-    <div className='input-block'>
-      {/* <label htmlFor={name}>{label}</label> */}
-      <input 
-        type={type} 
-        id={name} 
-        name={name} 
-        value={value} 
-        onChange={onChange} 
-        onBlur={onBlur} 
+    <div className="input-block">
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
         placeholder={placeholder}
+        ref={ref}
       />
       {error && <p>{error}</p>}
     </div>
-  )
+  );
 }
