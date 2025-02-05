600f93b89cd17a7c2eea02c0dca31034b8570ea0
jasmine <jasmigermano@gmail.com>
2025-02-05 17:44:57 -0300

    ✨ feat: add and style main menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
-'use client';
-import Logo from "@/assets/images/logo.svg";
+"use client";
+
+import React, { useContext, useEffect } from 'react'
+import { AuthContext } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
+import Logo from '@/assets/images/logo.svg';
+import HomeIcon from '@/assets/images/home.svg';
 import Image from "next/image";
-import { useContext, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { AuthContext } from "@/context/AuthContext";
+import LogoutIcon from '@/assets/images/logout.svg';
 
 export default function Home() {
-  const { user } = useContext(AuthContext);
+  const { user, email, logout, photo, name } = useContext(AuthContext);
   const router = useRouter();
 
   console.log(user);
@@ -19,12 +22,39 @@ export default function Home() {
 
   if (!user) return null;
 
+  function handleLogout() {
+    logout();
+    router.push("/login");
+  }
+
   return (
     <div>
-      <Image src={Logo} alt="Logo" />
-      <p>
-        <a href="/login">Login</a> | <a href="/register">Register</a>
-      </p>
+      <div className='menu'>
+        <Image src={Logo} alt="Logo" height={25} />
+        <div className='account'>
+          <div className='circle profile-picture'>
+            <img className='pfp' src={photo ? photo : 'https://secure.gravatar.com/avatar/729ae85bf62b9917e93538db2f2688ca?s=96&d=mm&r=g'} alt='foto de perfil' />
+          </div>
+          <div>
+            <p className='account-name'>{name}</p>
+            <p className='account-username'>@{user}</p>
+          </div>
+        </div>
+        <div className='menu-options'>
+          <button onClick={() => router.push("/")} className='menu-btn'>
+            <div className='menu-icon'>
+              <Image src={HomeIcon} alt='home' />
+            </div>
+            <span>home</span>
+          </button>
+          <button onClick={() => handleLogout()} className='menu-btn'>
+            <div className='menu-icon'>
+              <Image src={LogoutIcon} alt='logout' />
+            </div>
+            <span>sair</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
