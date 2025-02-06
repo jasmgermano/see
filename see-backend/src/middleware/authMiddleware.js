a62064632bc97b07b3a02326bcecb016effe1031
jasmine <jasmigermano@gmail.com>
2025-02-06 11:32:44 -0300

    🚀 feat: configure Express server

---
+++ see-backend/src/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
