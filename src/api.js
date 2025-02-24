18b87c6e83d7d23e7b70308a05d233ea670cf610
jasmine <jasmigermano@gmail.com>
2025-02-24 18:24:40 -0300

    ✨feat: add user update & profile endpoints

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export function TOKEN_POST(body) {
+  console.log("body", body);
   return {
     url: `${API_URL}/auth/login`,
     options: {
@@ -38,11 +39,52 @@ export function USER_POST(body) {
     },
   };
 }
+
 export function USER_GET_BY_USERNAME(username) {
   return {
     url: `${API_URL}/users/profile/${username}`,
     options: {
       method: "GET",
+    },
+  };
+}
+
+export function USER_PUT(token, body) {
+  return {
+    url: `${API_URL}/auth/update`,
+    options: {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function PROFILE_PICTURE_PUT(id, formData, token) {
+  return {
+    url: `${API_URL}/users/${id}/profile-picture`,
+    options: {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    },
+  };
+}
+
+export function PROFILE_COVER_PUT(id, formData, token) {
+  return {
+    url: `${API_URL}/users/${id}/profile-cover`,
+    options: {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
     },
   };
 }
